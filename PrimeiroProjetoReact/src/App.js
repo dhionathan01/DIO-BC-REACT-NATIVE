3a12fd42ffbd4b6af285232d7c86ba59ade0315a
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-06-28 20:34:04 -0300

    Importando foto do facebook e centralizando

--- PrimeiroProjetoReact/src/App.js
+++ PrimeiroProjetoReact/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import {View, Text, StyleSheet, sa, SafeAreaView, StatusBar} from 'react-native';
+import { View,Text, Image, StyleSheet, sa, SafeAreaView, StatusBar } from 'react-native';
+const corDeFundo = '#010409';
+const imagemProfileGithub = 'https://scontent-gig2-1.xx.fbcdn.net/v/t39.30808-6/290209625_4720268214746130_3519532189470720939_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEt5PqjwMrUndr9cKunnBObOo0KoxCud5k6jQqjEK53meDe5agZjpSi8nAxIbO3ZLb8dVVqxNUwMvzqGl2SZR0E&_nc_ohc=5YRGcO70-gIAX-0FItB&_nc_ht=scontent-gig2-1.xx&oh=00_AT-lJ9H-AhxPLzAI2_T5KviluHd_V72_Yq-eT00KgRY48Q&oe=62C14B33';
 const App = () => {
     return (
         <SafeAreaView style={style.container}>
-            <StatusBar backgroundColor={'lightsteelblue'}/>
-                <View>
-                    <Text style={style.text}>Hello World</Text>
-                </View>    
+            <StatusBar backgroundColor={corDeFundo} barStyle='light-content' />
+            <View style={style.content}>
+                <Image accessibilityLabel="Dhionathan Foto" style={style.avatar} source={{uri: imagemProfileGithub}}/>
+                <Text>Dhionathan Lanzoni Jobim</Text>
+                <Text>dhionathan01</Text>
+                <Text>
+                    Estudande de Desenvolvimento.
+                    Graduando em Análise e Desenvolvimento de Sistemas.
+                </Text>
+            </View>
         </SafeAreaView>
+
+        
+        
     );
 };
 
@@ -15,14 +26,26 @@ export default App;
 
 const style = StyleSheet.create({
     container: {
-        backgroundColor: 'lightsteelblue',
+        //Column
+        backgroundColor: corDeFundo,
         flex: 1, // Expandir para a tela inteira
+        justifyContent: 'center',
+        alignItems: 'center',
+        //flexDirection: 'row',
 
     },
 
-    text: {
-        fontSize: 50,
-        fontWeight: 'bold',
-        color: 'darkslategrey ',
+    content: {
+      alignItems: 'center',  
     },
+
+    avatar: {
+        height: 300,
+        width: 300,
+        borderColor: 'white',
+        borderRadius: 150,
+        borderWidth: 2,
+    },
+
+    
 });
